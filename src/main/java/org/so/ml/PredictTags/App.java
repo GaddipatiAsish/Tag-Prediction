399dfe6d2c3e91aac4a6baaac716f85724a9c239
unknown <gaddipati1991@gmail.com>
2015-04-15 16:16:59 -0700

    First commit

---
+++ src/main/java/org/so/ml/PredictTags/App.java
@@ -0,0 +1,13 @@
+package org.so.ml.PredictTags;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
