eb36a73d50563b8fe29868fd8a2f049631cd1ffd
unknown <inblueswithu@yahoo.co.in>
2015-04-18 21:33:21 -0400

    Added class to get aggregate tags list from the database

---
+++ src/main/java/org/so/ml/WebScraper/AggregateTagsList.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package org.so.ml.WebScraper;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.so.ml.core.DBAccess;
+
+/**
+ * Aggregates all the tags present in the database.
+ * We have fetched only top N tags and M questions for each of these
+ * tags. But for each question there could be utmost 5 tags. So, in
+ * out DB there could be N*5 tags. This program is to aggregate and 
+ * get all the unique tags.
+ * @author chinmaya
+ *
+ */
+public class AggregateTagsList {
+	
+	// DB
+	private static DBAccess db;
+	// HashMap to store the tags
+	private static HashMap<String, Integer> hashMap;
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		// Get to DB
+		db = new DBAccess();
+		db.connect("couchdb.properties");
+		// Run the view 
+		db.runView("all_docs/tag_and_question", 0);
+		// Initialize HashMap
+		hashMap = new HashMap<String, Integer>(500);
+		
+		// Add all tags to the hash map
+		long max=db.noOfRowsInView;
+		for(int t=0; t<max; t++) {
+			String tag = (String) db.viewResultGetKey(t, 0);
+			int value = 0;	// no of questions with this tag
+			if(hashMap.containsKey(tag))
+				value += hashMap.get(tag) + 1; 
+			else
+				value += 1;
+			hashMap.put(tag, value);
+		}
+		
+		// Write to file
+		writeToFile();
+	}
+
+	/**
+	 * Write the hashmap to file or just the tags
+	 * @throws IOException 
+	 */
+	private static void writeToFile() throws IOException {
+		// Iterate on hashmap and write to file
+		Iterator<Map.Entry<String, Integer>> iter = hashMap.entrySet().iterator();
+		BufferedWriter bw = new BufferedWriter(new FileWriter("./data/AggregateTags.result"));
+		while(iter.hasNext()) {
+			bw.write(iter.next().getKey() + "\n");		// change file to AggregateTags.result
+//			bw.write(iter.next().toString() + "\n");	// change file to AggregateTagsWithValues.result
+		}
+		// close buffer
+		bw.close();
+	}
+}
--- src/main/java/org/so/ml/WebScraper/TagGrabber.java
+++ src/main/java/org/so/ml/WebScraper/TagGrabber.java
@@ -3,25 +3,39 @@ package org.so.ml.WebScraper;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.jaunt.NotFound;
 import com.jaunt.ResponseException;
 import com.jaunt.UserAgent;
 
+/**
+ * Gets tags from the webpage and saves to a file
+ * @author chinmaya
+ */
 public class TagGrabber {
 
 	public static void main(String[] args) throws ResponseException, NotFound, IOException {
 		
 		// Get the page
 		UserAgent agent = new UserAgent();
-		agent.visit("http://stackoverflow.com/tags");
-		com.jaunt.Document webpage = agent.doc;
-		// get the tag list
-		WebsiteScraper ws = new WebsiteScraper();
-		List<String> tagList = ws.getAllTags(webpage);
+		String url = "http://stackoverflow.com/tags?page=";
+		
+		// Get top N tags
+		List<String> tagList = new ArrayList<String>();
+		int nTopTags = 100;
+		for(int t=0; t<nTopTags/33; ++t) {	// 36 tags per page but take 33 to get nearly 100 tags
+			url += t + "&tab=popular";
+			agent.visit(url);
+			com.jaunt.Document webpage = agent.doc;
+			// get the tag list
+			WebsiteScraper ws = new WebsiteScraper();
+			tagList.addAll(ws.getAllTags(webpage));
+		}
 		
 		// write to file
+		System.out.println("TagList File Generated");
 		writeTagsToFile(tagList);
 	}
 	
